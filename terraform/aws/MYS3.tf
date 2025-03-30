e09764c1195a9c09e3513a8cdcf3f14c0bae6a85
darnon81 <110915208+darnon81@users.noreply.github.com>
2025-03-30 11:43:36 +0300

    Update terraform/aws/MYS3.tf
    fix
    
    Co-authored-by: bridgecrew[bot] <60663194+bridgecrew[bot]@users.noreply.github.com>

--- terraform/aws/MYS3.tf
+++ terraform/aws/MYS3.tf
@@ -101,6 +101,56 @@ resource "aws_s3_bucket" "operations" {
 }
 
 
+resource "aws_s3_bucket_versioning" "operations" {
+  bucket = aws_s3_bucket.operations.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket" "destination" {
+  bucket = aws_s3_bucket.operations.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_iam_role" "replication" {
+  name = "aws-iam-role"
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "s3.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_s3_bucket_replication_configuration" "operations" {
+  depends_on = [aws_s3_bucket_versioning.operations]
+  role   = aws_iam_role.operations.arn
+  bucket = aws_s3_bucket.operations.id
+  rule {
+    id = "foobar"
+    status = "Enabled"
+    destination {
+      bucket        = aws_s3_bucket.destination.arn
+      storage_class = "STANDARD"
+    }
+  }
+}
+
+
+
+
 resource "aws_s3_bucket_server_side_encryption_configuration" "operations" {
   bucket = aws_s3_bucket.operations.bucket
 
